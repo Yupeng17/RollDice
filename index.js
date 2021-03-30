6d2f304e56c78cf98bcfd2f4490ef179377c1b81
Yupeng17 <80818545+Yupeng17@users.noreply.github.com>
2021-03-30 12:33:43 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function rollDice() {
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+console.log(randomNumber1);
+
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+console.log(randomNumber2);
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector('.img2').setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 === randomNumber2) {
+  document.querySelector(".heading").innerHTML = "Draw!";
+}
+else if (randomNumber1 > randomNumber2) {
+  document.querySelector(".heading").innerHTML = "🚩 Player1 Win!";
+}
+else if(randomNumber1 < randomNumber2) {
+  document.querySelector(".heading").innerHTML = "Player2 Win! 🚩";
+}
+}
